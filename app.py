a9ff04a8c5e0b19d9e1217fb43d702258616cefe
sj <sasangjin@gmail.com>
2022-07-13 10:49:58 +0900

    main페이지 적용입니다.

--- app.py
+++ app.py
@@ -16,34 +16,49 @@ db = client.dbsparta2
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('main.html')
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
 
 
-@app.route('/listing_cafe', methods=['GET'])
-def show_cafe():
-    cafes = list(db.cafe_mogakco.find({}, {'_id': False}))
-    return jsonify({'all_cafe': cafes})
+# @app.route('/listing_cafe', methods=['GET'])
+# def show_cafe():
+#     cafes = list(db.cafe_mogakco.find({}, {'_id': False}))
+#     return jsonify({'all_cafe': cafes})
 
 
 @app.route('/posting_cafe')
 def cafe():
     return render_template('posting_cafe.html')
 
+@app.route('/cafe', methods=['GET'])
+def show_cafe():
+    data = list(db.cafe.find({}, {'_id': False, 'lat': False, 'lng': False}))
+    return jsonify({'data': data})
 
-@app.route('/posting_cafe', methods=['POST'])
+@app.route("/cafe", methods=["POST"])
 def save_cafe():
-    cafe_name_receive = request.form['cafe_name_give']
-    cafe_address_receive = request.form['cafe_address_give']
-    today = datetime.now()
-    created_date = today.strftime('%Y-%m-%d')
+    name = request.form['cafe_name']
+    address = request.form['cafe_address']
+    lat = request.form['lat']
+    lng = request.form['lng']
+    rating = request.form['rating']
+    createdAt = datetime.now().strftime('%Y-%m-%d')
 
     doc = {
-        'cafe_name': cafe_name_receive,
-        'cafe_address': cafe_address_receive,
-        'date': created_date
+        'cafe_name': name,
+        'cafe_address': address,
+        'lat': lat,
+        'lng': lng,
+        'rating': rating,
+        'createdAt': createdAt
     }
-    db.cafe_mogakco.insert_one(doc)
-    return jsonify({'msg': '저장 완료!'})
+
+    db.cafe.insert_one(doc)
+
+    return jsonify({'msg': '모각코GOGO 추천 카페로 등록되었습니다.'})
 
 
 if __name__ == '__main__':
