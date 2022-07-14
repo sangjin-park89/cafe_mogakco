14c4f021f6dbc2eb58570f3935b34c43de1a39f5
wisdom <wisdom08@kakao.com>
2022-07-14 16:53:11 +0900

    feat: api 변경 및 ajax 변경

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ client = MongoClient(
     'mongodb+srv://byunjihye:asdf33@cluster0.qulah.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta2
 
+SECRET_KEY = 'SPARTA'
 
 # 메인페이지=카페목록페이지 보기
 @app.route('/')
@@ -28,7 +29,13 @@ def map():
 # 카페 목록 요청하는 api
 @app.route('/cafe', methods=['GET'])
 def show_cafe():
-    data = list(db.cafeList.find({}, {'_id': False, 'lat': False, 'lng': False}))
+    data = list(db.cafe.find({}, {'_id': False, 'lat': False, 'lng': False}))
+    return jsonify({'data': data})
+
+
+@app.route('/cafe/detail/<string:name>', methods=['GET'])
+def detail_cafe(name: str):
+    data = db.cafe.find_one({'name': name}, {'_id': False, 'lat': False, 'lng': False})
     return jsonify({'data': data})
 
 
@@ -46,9 +53,8 @@ def save_cafe():
     lat = request.form['lat']
     lng = request.form['lng']
     rating = request.form['rating']
-    # 후기에서 입력, 저장된 점수 디비에서 불러오기..
     createdAt = datetime.now().strftime('%Y-%m-%d')
-    
+
     doc = {
         'name': name,
         'address': address,
@@ -58,56 +64,62 @@ def save_cafe():
         'createdAt': createdAt
     }
 
-    db.cafeList.insert_one(doc)
+    db.cafe.insert_one(doc)
     return jsonify({'msg': '모각코 추천 카페로 등록되었습니다.'})
 
 
-# 특정카페 상세페이지 보기
-@app.route('/cafe/<string:name>')
+# 특정카페 후기 등록
+@app.route('/review/<string:name>')
 def show_detail(name: str):
     return render_template('review.html')
 
-
-# 후기 목록 요청하는 api
-@app.route('/cafe/<string:name>', methods=['GET'])
-def detail_cafe(name: str):
-    data = list(db.cafeList.find_one({'name': name}, {'_id': False, 'lat': False, 'lng': False}))
-    return jsonify({'data': data})
+# 특정카페 상세페이지 보기
+@app.route('/cafe/<string:name>')
+def cafe_detail(name: str):
+    return render_template('detail.html')
 
 
 # 특정카페 후기 등록하는 api
-@app.route("/cafe/<string:name>", methods=["POST"])
-def save_comment(name: str):
-    # userid = request.form['userid']
-    usability = request.form['usability']
-    soundmood = request.form['soundmood']
-    price = request.form['price']
-    comment = request.form['comment']
-    createdAt = datetime.now().strftime('%Y-%m-%d')
+@app.route("/comment", methods=["POST"])
+def save_comment():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({'username': payload['id']})
+        print(user_info)
+        userid = user_info['username']
+        name = request.form['name']
+        usability = request.form['usability']
+        mood = request.form['mood']
+        price = request.form['price']
+        comment = request.form['comment']
+        createdAt = datetime.now().strftime('%Y-%m-%d')
+        doc = {
+            'name': name,
+            'userid': userid,
+            'usability': usability,
+            'mood': mood,
+            'price': price,
+            'comment': comment,
+            'createdAt': createdAt,
+        }
+        db.comment.insert_one(doc)
+        return jsonify({'msg': '후기 등록 완료'})
 
-    doc = {
-        'name': name,
-        # 'userid': userid,
-        'usability':usability,
-        'soundmood':soundmood,
-        'price':price,
-        'comment':comment,
-        'createdAt': createdAt,
-    }
-    db.comment.insert_one(doc)
-    return jsonify({'msg': '후기 등록 완료'})
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 올바르지 않습니다."))
 
 
 # 정희님 담당쓰 건드리기가 무서워용
-
-
-
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-SECRET_KEY = 'SPARTA'
+
 @app.route('/')
 def go_home():
     token_receive = request.cookies.get('mytoken')
@@ -132,8 +144,8 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -143,20 +155,18 @@ def sign_in():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-
-
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -168,23 +178,6 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-
-
-
-@app.route("/comment/<string:name>", methods=["POST"])
-def save_comment(name: str):
-    userid = request.form['userid']
-    content = request.form['content']
-    createdAt = datetime.now().strftime('%Y-%m-%d')
-
-    doc = {
-        'name': name,
-        'userid': userid,
-        'content': content,
-        'createdAt': createdAt,
-    }
-    db.comment.insert_one(doc)
-    return jsonify({'msg': '댓글 등록 성공'})
 
 @app.route('/comment/<string:name>', methods=['GET'])
 def get_comments(name: str):
